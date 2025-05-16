a604077641c329d92123ba31e2db5ff93bbf0fcb
Alexis <180454027+Alexis5631@users.noreply.github.com>
2025-05-15 21:08:28 -0500

    feat :sparkles: create database

--- obj/Debug/net9.0/CampusLove2.AssemblyInfo.cs
+++ obj/Debug/net9.0/CampusLove2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusLove2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0938233b879ff6d25cfec53544b8c0c06b9ca1e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusLove2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusLove2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
