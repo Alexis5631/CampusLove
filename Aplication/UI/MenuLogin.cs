baeb6e5e00bc637ae4e7ef45058cce71fa1929d9
Alexis <180454027+Alexis5631@users.noreply.github.com>
2025-05-15 21:43:02 -0500

    feat: :sparkles: Domain and entities

---
+++ Aplication/UI/MenuLogin.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampusLove2.Aplication.UI
+{
+    public class MenuLogin
+    {
+        
+    }
+}
